fff47b7e9967d7aac77d672ec8c67cab1d195e72
Helder <helder.jesus@fatec.sp.gov.br>
2021-09-14 22:16:29 -0300

    Factories adicionadas

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,9 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
+
+        Autor: <a href="#">{{ $post->user->name }}</a> <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
         {!! $post->body !!}
     </article>
     <a href="/">Voltar</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,8 @@
     @foreach($posts as $post)
         <article>
             <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            Autor: <a href="#">{{ $post->user->name }}</a><br>
+            Categoria: <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
