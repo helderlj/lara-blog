08822c87294561c291e5f3aceac28c78fdc6ec2f
Helder <helder.jesus@fatec.sp.gov.br>
2021-09-20 21:12:47 -0300

    pesquisa implementada

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -17,20 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('post.show');
 
-Route::get('categories/{category:slug}', function (Category $category){
+Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
         'currentCategory' => $category,
@@ -38,7 +30,7 @@ Route::get('categories/{category:slug}', function (Category $category){
     ]);
 })->name('category');
 
-Route::get('authors/{author:username}', function (User $author){
+Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
         'categories' => Category::all()
